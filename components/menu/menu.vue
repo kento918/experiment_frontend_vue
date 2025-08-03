7f4c197171eb5a53f4be094e95e7914be4fee498
kento9182 <ewne.ne.cw@gmail.com>
2025-08-03 09:28:10 +0900

    add change component

---
+++ components/menu/menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+      <v-row>
+         test 
+      </v-row>
+    </v-container>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
-   <NuxtLayout>
-      <v-main>
-         <DragAndDropSample />
-      </v-main>
-   </NuxtLayout>
+  <NuxtLayout>
+    <v-btn @click="search = 1">Load Menu</v-btn>
+    <v-btn @click="search = 2">Load Drag and Drop Sample</v-btn>
+
+    <component :is="comp" :key="search"/>
+  </NuxtLayout>
 </template>
+
 <script setup>
-import DragAndDropSample from '@/components/sample/dragAndDropSample.vue'
+const search = ref(1)
+
+const comp = computed(() => {
+  if (search.value === 1) {
+    return defineAsyncComponent(() => import('@/components/menu/menu.vue'))
+  }
+  if (search.value === 2) {
+    return defineAsyncComponent(() =>
+      import('@/components/dragAndDropSample/dragAndDropSample.vue')
+    )
+  }
+  return null
+})
 </script>
-<style scoped>
-</style>
