3e8d7268bfd83cdcdf083ecb1696c6ddbdd15e04
kento9182 <ewne.ne.cw@gmail.com>
2025-12-23 22:36:14 +0900

    feat: api周りの追加

---
+++ composables/api.ts
@@ -0,0 +1,33 @@
+export const getRequest = async (url: string) => {
+   const res = await fetch(url);
+   return await res.json();
+};
+
+export const postRequest = async (url: string, data: any) => {
+   const res = await fetch(url, {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+   });
+   return await res.json();
+};
+
+export const putRequest = async (url: string, data: any) => {
+   const res = await fetch(url, {
+      method: "PUT",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+   });
+   return await res.json();
+};
+
+export const deleteRequest = async (url: string) => {
+   const res = await fetch(url, {
+      method: "DELETE",
+   });
+   return await res.json();
+};
